f5963a29cedf62c16ff00c68e4f64c16268ce90e
Naveen <naveen@squareshift.co>
2025-01-29 19:31:55 +0530

    Add warning message

--- sqshft-interview-portal-client/src/components/navbar/index.js
+++ sqshft-interview-portal-client/src/components/navbar/index.js
@@ -4,8 +4,15 @@ import "./index.css";
 
 export const Navbar = () => {
   return (
-    <div className="nav-bar-container">
-      <img src={IMAGE_URL} className="img" alt=" "></img>
-    </div>
+    <>
+      <div className="nav-bar-container">
+        <img src={IMAGE_URL} className="img" alt=" "></img>
+      </div>
+      <div style={{
+        color: "red",
+        padding: "20px 0 0 0",
+        textAlign: "center"
+      }}>Please stay on this tab!. If you try to change tab you will be logged out</div>
+    </>
   );
 };
